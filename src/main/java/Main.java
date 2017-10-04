b8e3108c1e422bb714698c8e8cbb3a34ef874bca
VeronikaAlifer <1109540@gmail.com>
2017-10-04 11:11:39 +0300

    Update game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,22 +6,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-	 public static void main(String[] args) throws IOException {
-	        Play play = new Play();
-	        List<String> cityList = new ArrayList<String>();
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		Play play = new Play();
+		List<String> usedCityList = new ArrayList<String>();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-	        while (true) {
-	            System.out.println("Ваш ход");
-	            String word = reader.readLine();
-	            play.checkExitString(word);
-	            boolean result = play.isCityUsed(word, cityList);
+		while (true) {
+			System.out.println("Твой ход:");
+			String userCity = reader.readLine();
+			play.checkExitString(userCity);
 
-	            if (result) {// если  System.out.println("Такой город уже был");результат отрицательный и в списке уже названных городов нет повторяющегося
-	                play.computersFindsCity(cityList, word);
-	            } else {
-	                System.out.println("Такой город уже был");
-	            }
-	        }
-	    }
+			boolean isCityExist = play.isCityInDictionary(userCity);
+			boolean isCityUsed = play.isCityUsed(userCity, usedCityList);
+			boolean isFirstLetterAppropriate = play.isFirstLetterAppropriate(userCity);
+
+			if (isCityExist) {
+				if (!isCityUsed && isFirstLetterAppropriate) {
+					usedCityList.add(userCity.toUpperCase());
+					play.computersFindsCity(usedCityList, userCity);
+				} else if (!isFirstLetterAppropriate) {
+					System.out.println("Пользователь, не мухлюй, введи город, который будет начинаться на ту букву, "
+							+ "которая была последней в моем городе!!!!");
+				} else {
+					System.out.println("Пользователь, такой город уже был, повтори попытку!!!!");
+				}
+			} else {
+				System.out.println("Пользователь, что за ерунду ты пишешь, такого города не существует!!!!");
+			}
+		}
+	}
 }
